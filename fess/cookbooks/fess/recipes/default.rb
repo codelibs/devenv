dd60d981a279884ac9986494d32ce078832c48dd
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-02-15 14:50:50 +0900

    add mysql support for fess

--- fess/cookbooks/fess/recipes/default.rb
+++ fess/cookbooks/fess/recipes/default.rb
@@ -1,7 +1,10 @@
-version = '9.0.0-SNAPSHOT'
+version = '9.0.2'
+#version = '9.0.0-1'
 db = 'h2'
+#db = 'mysql'
 filename = "fess-server-#{db}-#{version}.noarch.rpm"
 remote_uri = "http://fess.codelibs.org/snapshot/#{filename}"
+mysql_config = "/root/fess_mysql_config"
 
 remote_file "/tmp/#{filename}" do
     source "#{remote_uri}"
@@ -19,3 +22,16 @@ service "fess" do
     action [:enable, :start]
     supports :status => true, :restart => true, :reload => true
 end
+
+bash "mysql_config" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  echo "FESS_PASSWORD=fessdb" > #{mysql_config}
+  echo "ROBOT_PASSWORD=robotdb" >> #{mysql_config}
+  echo "MYSQL_PASSWORD=mysqldb" >> #{mysql_config}
+  bash /opt/fess/extension/mysql/install.sh #{mysql_config}
+  EOH
+  not_if { ::File.exists?(mysql_config) }
+end
+

a04d700bdfef3e2668e1cede470cb1a5fb3800ca
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-04-18 21:09:46 +0900

    update to 1.1.1 and Java 8

--- elasticsearch/cookbooks/elasticsearch/recipes/default.rb
+++ elasticsearch/cookbooks/elasticsearch/recipes/default.rb
@@ -1,4 +1,4 @@
-filename = "elasticsearch-1.0.2.noarch.rpm"
+filename = "elasticsearch-1.1.1.noarch.rpm"
 remote_uri = "https://download.elasticsearch.org/elasticsearch/elasticsearch/#{filename}"
 
 service "elasticsearch" do

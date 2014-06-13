27b0093030f681e12c99c7efb80556d010457bd8
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-06-13 14:37:35 +0900

    elasticsearch 1.2.1

--- elasticsearch/cookbooks/elasticsearch/recipes/default.rb
+++ elasticsearch/cookbooks/elasticsearch/recipes/default.rb
@@ -1,4 +1,4 @@
-filename = "elasticsearch-1.2.0.noarch.rpm"
+filename = "elasticsearch-1.2.1.noarch.rpm"
 remote_uri = "https://download.elasticsearch.org/elasticsearch/elasticsearch/#{filename}"
 
 service "elasticsearch" do

987a35d8bc5f87947cb4ed3bb15386d06f0abfd8
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-11-29 07:00:22 +0900

    add http.jsonp.enable

--- elasticsearch/cookbooks/elasticsearch/recipes/default.rb
+++ elasticsearch/cookbooks/elasticsearch/recipes/default.rb
@@ -37,11 +37,12 @@ bash "update_es_yml" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  cat /etc/elasticsearch/elasticsearch.yml | grep -v cluster.name | grep -v node.name | grep -v index.number_of_shards | grep -v index.number_of_replicas > /tmp/elasticsearch.yml.tmp
+  cat /etc/elasticsearch/elasticsearch.yml | grep -v cluster.name | grep -v node.name | grep -v index.number_of_shards | grep -v index.number_of_replicas | grep -v http.jsonp.enable > /tmp/elasticsearch.yml.tmp
   echo "cluster.name: elasticsearch-codelibs" >> /tmp/elasticsearch.yml.tmp
   echo "node.name: \"ES Node 1\"" >> /tmp/elasticsearch.yml.tmp
   echo "index.number_of_shards: 1" >> /tmp/elasticsearch.yml.tmp
   echo "index.number_of_replicas: 0" >> /tmp/elasticsearch.yml.tmp
+  echo "http.jsonp.enable: true" >> /tmp/elasticsearch.yml.tmp
   mv -f /tmp/elasticsearch.yml.tmp /etc/elasticsearch/elasticsearch.yml
   sed -e "s/es.logger.level: INFO/es.logger.level: DEBUG/" /etc/elasticsearch/logging.yml > /tmp/logging.yml.tmp
   mv -f /tmp/logging.yml.tmp /etc/elasticsearch/logging.yml

9119381302257900b2049d875c1ea1647e6958f2
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-02-21 16:39:57 +0900

    remove log files

--- elasticsearch/bin/restart_es.sh
+++ elasticsearch/bin/restart_es.sh
@@ -6,5 +6,6 @@ if [ x$1 = "xwait" ] ; then
 fi
 
 vagrant ssh -c "sudo /etc/init.d/elasticsearch stop"
+rm ../data/elasticsearch/logs/elasticsearch-codelibs*
 vagrant ssh -c "sudo ES_JAVA_OPTS=\"-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=$SUSPEND\" /etc/init.d/elasticsearch start"
 

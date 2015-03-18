5cecbd2d69f0d5d8fc6556501dd6f13f3a03405c
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-03-18 22:46:06 +0900

    refactoring

--- mecab-ipadic-neologd/site-cookbooks/mecab-ipadic-neologd/recipes/default.rb
+++ mecab-ipadic-neologd/site-cookbooks/mecab-ipadic-neologd/recipes/default.rb
@@ -10,7 +10,13 @@ ipadic_url = "http://mecab.googlecode.com/files/#{ipadic_file}"
 neologd_git_url = "https://github.com/neologd/mecab-ipadic-neologd.git"
 lucene_version = "4_10_3"
 lucene_svn_url = "http://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_#{lucene_version}"
-
+resource_process_file = "/tmp/resouce_process.txt"
+mecab_process_file = "/tmp/mecab_process.txt"
+ipadic_process_file = "/tmp/ipadic_process.txt"
+neologd_process_file = "/tmp/neologd_process.txt"
+lucene_process_file = "/tmp/lucene_process.txt"
+kuromoji_process_file = "/tmp/kuromoji_process.txt"
+max_baseform_length = 15
 
 remote_file "/tmp/#{ant_file}" do
     source "#{ant_url}"
@@ -42,7 +48,97 @@ end
     end
 end
 
-bash "mysql_config" do
+bash "setup-resources" do
+  user "vagrant"
+  cwd "/home/vagrant"
+  code <<-EOH
+  export MECAB_HOME=`pwd`/mecab_home
+
+  mkdir $MECAB_HOME
+
+  tar zxvf /tmp/#{ant_file}
+  tar zxvf /tmp/#{mecab_file}
+  tar zxvf /tmp/#{ipadic_file}
+
+  touch #{resource_process_file}
+  EOH
+  only_if { ! ::File.exists?(resource_process_file) }
+end
+
+bash "mecab-build" do
+  user "vagrant"
+  cwd "/home/vagrant"
+  code <<-EOH
+  export MECAB_HOME=`pwd`/mecab_home
+  export PATH=$MECAB_HOME/bin:$PATH
+
+  cd mecab-#{mecab_version}
+  ./configure --prefix=$MECAB_HOME
+  make
+  make install
+
+  touch #{mecab_process_file}
+  EOH
+  only_if { ! ::File.exists?(mecab_process_file) }
+end
+
+bash "ipadic-build" do
+  user "vagrant"
+  cwd "/home/vagrant"
+  code <<-EOH
+  export MECAB_HOME=`pwd`/mecab_home
+  export PATH=$MECAB_HOME/bin:$PATH
+
+  cd mecab-ipadic-#{ipadic_version}
+  ./configure --with-charset=utf-8
+  make
+  make install
+
+  touch #{ipadic_process_file}
+  EOH
+  only_if { ! ::File.exists?(ipadic_process_file) }
+end
+
+bash "neologd-build" do
+  user "vagrant"
+  cwd "/home/vagrant"
+  code <<-EOH
+  export MECAB_HOME=`pwd`/mecab_home
+  export NEOLOGD_HOME=`pwd`/mecab-ipadic-neologd
+  export PATH=$MECAB_HOME/bin:$PATH
+
+  rm -rf mecab-ipadic-neologd
+  git clone #{neologd_git_url}
+  cd mecab-ipadic-neologd
+  ./bin/install-mecab-ipadic-neologd -y -n --max_baseform_length #{max_baseform_length}
+
+  touch #{neologd_process_file}
+  EOH
+  only_if { ! ::File.exists?(neologd_process_file) }
+end
+
+bash "lucene-build" do
+  user "vagrant"
+  cwd "/home/vagrant"
+  code <<-EOH
+  export ANT_HOME=`pwd`/apache-ant-#{ant_version}
+  export LUCENE_SRC_HOME=`pwd`/lucene_solr_#{lucene_version}
+
+  export PATH=$ANT_HOME/bin:$PATH
+
+  rm -rf $LUCENE_SRC_HOME
+  svn co #{lucene_svn_url}
+  cd $LUCENE_SRC_HOME/lucene
+  ant ivy-bootstrap
+  ant compile
+
+  rm #{kuromoji_process_file}
+  touch #{lucene_process_file}
+  EOH
+  only_if { ! ::File.exists?(lucene_process_file) }
+end
+
+bash "kuromoji-build" do
   user "vagrant"
   cwd "/home/vagrant"
   code <<-EOH
@@ -54,35 +150,10 @@ bash "mysql_config" do
   export PATH=$ANT_HOME/bin:$PATH
   export PATH=$MECAB_HOME/bin:$PATH
 
-  mkdir $MECAB_HOME
-
-  tar zxvf /tmp/#{ant_file}
-  tar zxvf /tmp/#{mecab_file}
-  tar zxvf /tmp/#{ipadic_file}
-
-  cd mecab-#{mecab_version}
-  ./configure --prefix=$MECAB_HOME
-  make
-  make install
-  cd ..
-
-  cd mecab-ipadic-#{ipadic_version}
-  ./configure --with-charset=utf-8
-  make
-  make install
-  cd ..
-
-  git clone #{neologd_git_url}
-  cd mecab-ipadic-neologd
-  ./bin/install-mecab-ipadic-neologd -y -n --max_baseform_length 15
-  cd ..
-
-  svn co #{lucene_svn_url}
-  cd $LUCENE_SRC_HOME/lucene
-  ant ivy-bootstrap
-  ant compile
-
-  cd analysis/kuromoji
+  cd $LUCENE_SRC_HOME/lucene/analysis/kuromoji
+  ant clean
+  rm -rf src/ build.xml
+  svn update
 
   cp -Rp $NEOLOGD_HOME/build/mecab-ipadic-*-neologd-* $LUCENE_SRC_HOME/lucene/build/analysis/kuromoji
 
@@ -92,9 +163,28 @@ bash "mysql_config" do
   sed -i "s/euc-jp/utf-8/g" build.xml
   sed -i "s/, download-dict//g" build.xml
   sed -i "s/1g/2g/g" build.xml
+  sed -i "s/org\\/apache\\/lucene\\/analysis\\/ja/org\\/codelibs\\/lucene\\/analysis\\/ja/g" build.xml
+  perl -pi -e "s/org\\.apache\\.lucene\\.analysis\\.ja/org.codelibs.lucene.analysis.ja/g" `find . -type f | grep -v /\.svn/`
+  mv src/resources/org/apache src/resources/org/codelibs
 
   ant regenerate
   ant jar-core
+
+  touch #{kuromoji_process_file}
   EOH
+  only_if { ! ::File.exists?(kuromoji_process_file) }
 end
 
+bash "kuromoji-deploy" do
+  user "vagrant"
+  cwd "/home/vagrant"
+  code <<-EOH
+  export MECAB_HOME=`pwd`/mecab_home
+  export NEOLOGD_HOME=`pwd`/mecab-ipadic-neologd
+  export LUCENE_SRC_HOME=`pwd`/lucene_solr_#{lucene_version}
+
+  NEOLOGD_VERSION=`echo $NEOLOGD_HOME/seed/mecab-user-dict-seed.*.csv.xz | sed -e "s/.*seed\\.//" -e "s/.csv.xz//"`
+  NEOLOGD_LUCENE_JAR=`basename $LUCENE_SRC_HOME/lucene/build/analysis/kuromoji/lucene-analyzers-kuromoji-*.jar |sed -e "s/-SNAPSHOT//" -e "s/\\.jar/-${NEOLOGD_VERSION}.jar/" -e "s/analyzers-kuromoji/analyzers-kuromoji-neologd/"`
+  cp $LUCENE_SRC_HOME/lucene/build/analysis/kuromoji/lucene-analyzers-kuromoji-*.jar /opt/mecab-ipadic-neologd/$NEOLOGD_LUCENE_JAR
+  EOH
+end

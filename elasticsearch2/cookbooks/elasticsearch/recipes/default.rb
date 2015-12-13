2dadf4b307e5990466ab5bc14e033b382ff6d805
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-12-14 06:34:46 +0900

    update to es 2.1

--- elasticsearch2/cookbooks/elasticsearch/recipes/default.rb
+++ elasticsearch2/cookbooks/elasticsearch/recipes/default.rb
@@ -1,4 +1,4 @@
-es_version = "2.0.0-beta1"
+es_version = "2.1.0"
 filename = "elasticsearch-#{es_version}.rpm"
 remote_uri = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/#{es_version}/#{filename}"
 
@@ -33,12 +33,13 @@ bash "update_es_yml" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  cat /etc/elasticsearch/elasticsearch.yml | grep -v cluster.name | grep -v node.name | grep -v index.number_of_shards | grep -v index.number_of_replicas > /tmp/elasticsearch.yml.tmp
+  cat /etc/elasticsearch/elasticsearch.yml > /tmp/elasticsearch.yml.tmp
   echo "cluster.name: elasticsearch-codelibs" >> /tmp/elasticsearch.yml.tmp
   echo "node.name: \"ES Node 1\"" >> /tmp/elasticsearch.yml.tmp
   echo "index.number_of_shards: 1" >> /tmp/elasticsearch.yml.tmp
   echo "index.number_of_replicas: 0" >> /tmp/elasticsearch.yml.tmp
   echo "http.cors.enabled: true" >> /tmp/elasticsearch.yml.tmp
+  echo 'http.cors.allow-origin: "*"' >> /tmp/elasticsearch.yml.tmp
   mv -f /tmp/elasticsearch.yml.tmp /etc/elasticsearch/elasticsearch.yml
   sed -e "s/es.logger.level: INFO/es.logger.level: DEBUG/" /etc/elasticsearch/logging.yml > /tmp/logging.yml.tmp
   mv -f /tmp/logging.yml.tmp /etc/elasticsearch/logging.yml

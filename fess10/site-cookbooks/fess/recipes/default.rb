ec085757b8bab61816a47c26095331f0a95b6b2c
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-05-04 15:20:55 +0900

    modify remote url

--- fess10/site-cookbooks/fess/recipes/default.rb
+++ fess10/site-cookbooks/fess/recipes/default.rb
@@ -6,42 +6,38 @@ end
 
 case node['platform']
 when "ubuntu", "debian"
-  filename = "fess.deb"
-  # remote_uri = "http://fess.codelibs.org/snapshot/#{filename}"
+  filename = "fess-#{version}.rpm"
+  remote_uri = "http://fess.codelibs.org/snapshot/#{filename}"
 
-  # remote_file "/tmp/#{filename}" do
-  #     source "#{remote_uri}"
-  #     mode 00644
-  # end
-
-  cookbook_file "/tmp/#{filename}" do
-    source "fess.deb"
+  remote_file "/tmp/#{filename}" do
+   source "#{remote_uri}"
+   mode 00644
   end
 
   package "fess" do
-      action :install
-      source "/tmp/#{filename}"
-      options "--force-depends"
-      provider Chef::Provider::Package::Dpkg
-      notifies :restart, resources(:service => "fess")
-      notifies :enable, resources(:service => "fess")
+   action :install
+   source "/tmp/#{filename}"
+   options "--force-depends"
+   provider Chef::Provider::Package::Dpkg
+   notifies :restart, resources(:service => "fess")
+   notifies :enable, resources(:service => "fess")
   end
 when "centos", "redhat"
   filename = "fess-#{version}.rpm"
   remote_uri = "http://fess.codelibs.org/snapshot/#{filename}"
 
   remote_file "/tmp/#{filename}" do
-      source "#{remote_uri}"
-      mode 00644
+   source "#{remote_uri}"
+   mode 00644
   end
 
   package "fess" do
-      action :install
-      source "/tmp/#{filename}"
-      options "--nodeps"
-      provider Chef::Provider::Package::Rpm
-      notifies :restart, resources(:service => "fess")
-      notifies :enable, resources(:service => "fess")
+   action :install
+   source "/tmp/#{filename}"
+   options "--nodeps"
+   provider Chef::Provider::Package::Rpm
+   notifies :restart, resources(:service => "fess")
+   notifies :enable, resources(:service => "fess")
   end
 end
 

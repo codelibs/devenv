b9f3c4872d174de1992393dc5e7d6dfee5b81585
Shinsuke Sugaya <shinsuke@apache.org>
2019-05-01 18:15:14 +0900

    update java11

--- fess13/site-cookbooks/java/recipes/default.rb
+++ fess13/site-cookbooks/java/recipes/default.rb
@@ -8,15 +8,16 @@ when "ubuntu", "debian"
       action :install
     end
   when "11"
-    apt_package 'openjdk-11-jdk' do
-      action :install
-      options '-o Dpkg::Options::="--force-overwrite"'
-    end
     bash 'use_java11' do
       user 'root'
       cwd '/tmp'
       code <<-EOH
-      update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
+      curl -L -o openjdk-11.tar.gz "https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?openjdk_impl=hotspot&os=linux&arch=x64&release=latest&type=jdk"
+      tar xzvf openjdk-11.tar.gz
+      mkdir -p /usr/lib/jvm
+      mv jdk-11* /usr/lib/jvm
+      update-alternatives --install /usr/bin/java java `ls /usr/lib/jvm/jdk-11*/bin/java` 1
+      update-alternatives --set java `ls /usr/lib/jvm/jdk-11*/bin/java`
       EOH
     end
   end
